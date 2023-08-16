79b311a5202a78effd5bc08a6219c248943819fe
Krasnov-Alex <133637852+Krasnov-Alex@users.noreply.github.com>
2023-08-16 16:17:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
+import os
+
+
 class Pydb():
   def __init__(self, name):
-    self.name = name
-    with open((name + '.pydb'), 'a') as f:
-      pass
+    self.name = name + '.sql'
+    self.temp = []
+    with open((self.name), 'a+') as f:
+      if os.stat(self.name).st_size <= 0:
+        with open('pattern.txt', 'r') as f1:
+          for line in f:
+            self.temp.append(line)
+          if len(self.temp) < 1:
+            for line in f1:
+              f.write(line)
+            self.temp = []
 
   def add(self, key, volume):
-    self.check(key)
+    if type(key) != int:
+      self.check(key)
     self.check(volume)
     new_add = (key + ' ' + volume + '\n')
-    with open((self.name + '.pydb'), 'a') as f:
-      f.writelines(new_add)
+    with open((self.name), 'a') as fa:
+      fa.writelines(new_add)
+
+  def repl(self):
+    if os.stat(self.name).st_size > 0:
+      with open(self.name) as fr:
+        first_line = fr.readline().strip('\n')
+        new_line = str(int(first_line) + 1) + '\n'
+        a=fr.readlines()
+        a.insert(0, new_line)
+        with open(self.name, 'w') as file:
+          file.writelines(a)
 
   def check(self, vol):
     a = vol.split()
